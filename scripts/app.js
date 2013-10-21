b9aedec010ff66aacef3d4c1b6e4d64826fa52aa
Lewis <lewis@ubuntu.(none)>
2013-10-21 16:16:00 -0700

    asdf

--- scripts/app.js
+++ scripts/app.js
@@ -6,21 +6,6 @@ var app = angular.module('mainApp', [])
       .when('/', {
         templateUrl: 'views/main.html'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html'
-      })
-      .when('/learn', {
-        templateUrl: 'views/learn.html'
-      })
-      .when('/workouts', {
-        templateUrl: 'views/workouts.html'
-      })
-      .when('/events', {
-        templateUrl: 'views/event.html'
-      })
-      .when('/test', {
-        templateUrl: 'views/bartest.html'
-      })
       .otherwise({
         redirectTo: '/'
       });
