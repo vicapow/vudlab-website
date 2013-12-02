2937f265d4c814f4dc68e661a439b924f33f25a9
Lewis <lewis@ubuntu.(none)>
2013-12-02 15:50:33 -0800

    deleted a bunch of crap

--- sfpark1/dc/explore.R
+++
@@ -1,7 +0,0 @@
-setwd("/home/lewis/Documents/github/sfpark/dc")
-data  <- read.csv("pems_output.csv")
-
-head(data)
-
-data2  <- read.csv("download.csv")
-head(data2)
--- sfpark1/map/js/polyfills.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  var requestAnimationFrame = window.requestAnimationFrame 
-    || window.mozRequestAnimationFrame 
-    || window.webkitRequestAnimationFrame 
-    || window.msRequestAnimationFrame
-  window.requestAnimationFrame = requestAnimationFrame
-})()
